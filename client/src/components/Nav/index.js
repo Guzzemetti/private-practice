f04dc69307c51917474866f97a7b234e35dabecd
Phillip48 <phillippereira02@gmail.com>
2022-06-05 16:28:02 -0400

    Installed react strap and made the component for category cards. Added a placeholder profile pic.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -70,6 +70,7 @@ const Navbar = () => {
 
         <div className="navbar-section-nav">
           {showNavigation}
+          
         </div>
 
       </section>
@@ -77,55 +78,54 @@ const Navbar = () => {
       <section className="navbar-categories">
         <div className="option-for-1">
           <p className="category-options-navbar">Gaming</p>
-          <div className="on-hover-show-options">
+          {/* <div className="on-hover-show-options">
             <Link to='/'><button className="subcategory-button-on-hover">RPG</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">MOBA</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">MMO</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">FPS</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Other</button></Link>
-          </div>
+          </div> */}
         </div>
 
         <div className="option-for-1">
           <p className="category-options-navbar">Home</p>
-          <div className="on-hover-show-options">
+          {/* <div className="on-hover-show-options">
             <Link to='/'><button className="subcategory-button-on-hover">Cooking</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">DIY Projects</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Gardening</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Maintenance</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Other</button></Link>
-          </div>
+          </div> */}
         </div>
 
         <div className="option-for-1">
           <p className="category-options-navbar">Lifestyle</p>
-          <div className="on-hover-show-options">
+          {/* <div className="on-hover-show-options">
             <Link to='/'><button className="subcategory-button-on-hover">Fitness</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Fashion</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Selfcare</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Life Coaching</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Other</button></Link>
-          </div>
+          </div> */}
         </div>
 
         <div className="option-for-1">
           <p className="category-options-navbar">Tech</p>
-          <div className="on-hover-show-options">
+          {/* <div className="on-hover-show-options">
             <Link to='/'><button className="subcategory-button-on-hover">Computer Learning</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Programming</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">IT</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Streaming</button></Link>
             <Link to='/'><button className="subcategory-button-on-hover">Other</button></Link>
-            {/* sub-category: computer learning, programming, IT, streaming, other*/}
-          </div>
+          </div> */}
         </div>
 
 
         <div className="option-for-1">
           <p className="category-options-navbar">Misc.</p>
-          <div className="on-hover-show-options">
+          {/* <div className="on-hover-show-options">
             <Link to='/'><button className="subcategory-button-on-hover">Misty-Misc</button></Link>
-          </div>
+          </div> */}
         </div>
       </section>
     </header>
---
+++ client/src/components/categoryCards/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './category.css';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+// import { Link } from "react-router-dom";
+import tempPic from '../../assets/blank-profile-picture-973460__340.webp';
+
+// import LogoHeader from '../../assets/Private Practice-logos_transparent.png';
+
+const Category = () => {
+
+    // In Category page Map over everything and pass props to get all the instances of whats here from the DB
+    // Img height is in px might have to chnage to make it more dynamic
+    return (
+        <section className="section-hold-all-caetgories-options">
+            <div className="category-header-with-img">
+                <h1>The Category they choose</h1>
+            </div>
+            <div className="holds-cards">
+                <Card className="card-margin-top">
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            {/* Replace with dynamic title */}
+                            Categories will go here
+                        </CardTitle>
+                        {/* This might not be needed */}
+                        <CardSubtitle className="mb-2 text-muted" tag="h6">
+                            Card subtitle
+                        </CardSubtitle>
+                        <CardText className="card-text-with-img">
+                            <div className="card-text-side">
+                                Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.  Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </div>
+                            <div className="card-img-side">
+                                <img src={tempPic} className='img-width-for-profile' alt='Profile img'></img>
+                            </div>
+                        </CardText>
+                        <button className="card-button">See more</button>
+                    </CardBody>
+                </Card>
+            </div>
+
+        </section>
+    )
+}
+
+
+export default Category;
