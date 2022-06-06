467bdeddcd39821f20f130391c1907e736c04dbb
Austin Donovan <AustinLDonovan@gmail.com>
2022-06-06 15:05:58 -0400

    profile page components added to components folder. Render full profile from full profile folder other components are not functioning

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import ProductList from "../components/ProductList";
+// import CategoryMenu from "../components/CategoryMenu";
+// import Cart from "../components/Cart";
+// import Profilepicture from "../components/Profile/Profilepicture";
+// import Profilecard from "../components/Profile/Profilecard";
+import Fullprofile from "../components/Profile/Fullprofile";
+
+const Profile = () => {
+  return (
+    <>
+      {/* <Profilepicture />
+      <Profilecard /> */}
+      <Fullprofile />
+    </>
+  );
+};
+
+export default Profile;
