e88e27bbb5a2107b90af9e0f4ee2be135625482e
Phillip48 <phillippereira02@gmail.com>
2022-06-06 12:38:19 -0400

    Added index to export models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Categories = require('./Categories');
+const Lesson = require('./Lesson');
+const Review = require('./Lesson');
+
+module.exports = { User, Categories, Lesson, Review };
