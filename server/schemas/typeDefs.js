938700045a0cf598a415b22d34cddebf2b03c16d
Phillip48 <phillippereira02@gmail.com>
2022-06-07 15:21:52 -0400

    Update mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -80,7 +80,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!, profileImage: String, aboutMe: String): Auth
+    addUser(firstname: String!, lastname: String!, email: String!, password: String!, profileImage: String, aboutMe: String): Auth
     addOrder(lessons: [ID]!): Order
     updateUser(firstName: String, lastName: String, email: String, password: String, profileImage: String, aboutMe: String): User
     updateLesson(_id: ID!, title: String, description: String, user: String, price: Float, coach: String, ): Lesson
