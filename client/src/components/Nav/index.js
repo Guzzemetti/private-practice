cbbbb6e5c46f46c6e0f9cb368e2f06aa5cf4be01
John Hysong <johnahysong@gmail.com>
2022-06-07 12:31:16 -0400

    added categories to home, minor changes to Nav, added CategoryDropdown comp

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -73,7 +73,7 @@ const Navbar = () => {
         <div className="navbar-section-nav">
                     {/* <Link to="/signup"><button className="navbar-button">Signup</button></Link>
           <div className="div-padding"></div> */}
-          <Link to="/explore"><button className="navbar-button">Explore</button></Link>
+          <Link to="/explore"><button className="navbar-button">About</button></Link>
           <Link to="/login"><button className="navbar-button">Login</button></Link>
           <AiOutlineShoppingCart className="cart-navbar-icon" />
           {showNavigation}
@@ -82,59 +82,7 @@ const Navbar = () => {
 
       </section>
       {/* Category and sub category options */}
-      <section className="navbar-categories">
-        <div className="option-for-1">
-          <p className="category-options-navbar">Gaming</p>
-          {/* <div className="on-hover-show-options">
-            <Link to='/'><button className="subcategory-button-on-hover">RPG</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">MOBA</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">MMO</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">FPS</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Other</button></Link>
-          </div> */}
-        </div>
-
-        <div className="option-for-1">
-          <p className="category-options-navbar">Home</p>
-          {/* <div className="on-hover-show-options">
-            <Link to='/'><button className="subcategory-button-on-hover">Cooking</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">DIY Projects</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Gardening</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Maintenance</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Other</button></Link>
-          </div> */}
-        </div>
-
-        <div className="option-for-1">
-          <p className="category-options-navbar">Lifestyle</p>
-          {/* <div className="on-hover-show-options">
-            <Link to='/'><button className="subcategory-button-on-hover">Fitness</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Fashion</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Selfcare</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Life Coaching</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Other</button></Link>
-          </div> */}
-        </div>
-
-        <div className="option-for-1">
-          <p className="category-options-navbar">Tech</p>
-          {/* <div className="on-hover-show-options">
-            <Link to='/'><button className="subcategory-button-on-hover">Computer Learning</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Programming</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">IT</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Streaming</button></Link>
-            <Link to='/'><button className="subcategory-button-on-hover">Other</button></Link>
-          </div> */}
-        </div>
-
-
-        <div className="option-for-1">
-          <p className="category-options-navbar">Misc.</p>
-          {/* <div className="on-hover-show-options">
-            <Link to='/'><button className="subcategory-button-on-hover">Misty-Misc</button></Link>
-          </div> */}
-        </div>
-      </section>
+      
     </header>
   )
 };
