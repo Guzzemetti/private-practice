07a45e1cbe7ac484687318838f9146110a60b35d
John Hysong <johnahysong@gmail.com>
2022-06-08 02:03:15 -0400

    added crude layout for lesson details

--- client/src/components/LessonDetails/index.js
+++ client/src/components/LessonDetails/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "../LessonDetails/lessonDetails.css";
+import Gaming from "../../assets/gaming_category.png";
 
 const LessonDetails = () => {
   return (
-    <div>lesson details</div>
+    <div className="d">
+      <div className="d-row">
+      <div className="d-left">
+          <div className="d-image">
+            <img src={Gaming} alt=""></img>
+          </div>
+        </div>
+        <div className="d-right">
+          <div className="d-right-wrapper">
+            <h2>Lesson Title</h2>
+              <div className="d-coach">Coach Name</div>
+              <div className='d-desc'>
+              description description description description description description description description description description description description description description description description description description description.</div>
+              <div className='d-price'>$100/hr</div>
+              <div className='d-quantity'>Quanity left</div>
+              <button>Add To Cart</button>
+          </div>
+        </div>
+        
+      </div>
+
+      <p>Go Back</p>
+    </div>
   )
 }
 
