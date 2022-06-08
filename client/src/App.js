94612bb6c4e6fa1e7436070a22446b145a93adf8
John Guzzetta <johnguzzettaiv@gmail.com>
2022-06-08 00:19:38 -0400

    Updated the link for the About in the Nav to link to the about Path. Updated comments in app.js, removed unnecessary imports, refactored for readability.

--- client/src/App.js
+++ client/src/App.js
@@ -7,31 +7,31 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+
 import { setContext } from '@apollo/client/link/context';
-
 import { StoreProvider } from './utils/GlobalState';
+
+// App.css is needed to keep footer stuck at bottom.
+import "../src/App.css"
+
+//-------------------------------- Pages and Components ------------------------------------
 // Pages to import
-
-// import Profile from "../src/pages/Profile";
-// import Home from './pages/Home';
-
-// import Detail from './pages/Detail';
-// import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+// import Profile from "../src/pages/Profile";
+// import Home from './pages/Home';
+// import Detail from './pages/Detail';
+// import NoMatch from './pages/NoMatch';
 // import Success from './pages/Success';
 // import OrderHistory from './pages/OrderHistory';
 
 // Components to import
 import Navbar from "../src/components/Nav/index";
 import HomeComp from "../src/components/Home/index";
-// import Footer from ''; --If we want a footer... maybe only use in the home page?
-// import Categories from '../src/components/categoryCards/index';
-// import Nav from './components/Nav';
 import Footer from "../src/components/Footer/index";
+// import Categories from '../src/components/categoryCards/index';
+//------------------------------------------------------------------------------------------
 
-// App.css is needed to keep footer stuck at bottom.
-import "../src/App.css"
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -52,12 +52,15 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+
+//-------------------------------- App function that builds our webpages ------------------------------------
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
         <main>
           <StoreProvider>
+            {/* The Navbar is added here to be added to every page that renders */}
             <Navbar />
             <Routes>
               <Route 
@@ -85,13 +88,14 @@ function App() {
                 path="/products/:id" 
                 element={<NA />} 
               /> */}
-              {/* 404 */}
+              {/* 404 Page */}
               {/* <Route
                 path="*" 
                 element={<NA />} 
               /> */}
             </Routes>
           </StoreProvider>
+          {/* Renders the footer to the bottom of each page */}
           <Footer/>
         </main>
       </Router>
@@ -100,32 +104,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
