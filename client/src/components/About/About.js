645b06810a215ac4e2a34969339ab2ffe83d66ca
John Hysong <johnahysong@gmail.com>
2022-06-09 01:04:30 -0400

    about changes

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -8,6 +8,7 @@ import Austin from '../../assets/about/austin.png';
 import JohnG from '../../assets/about/johnG.png';
 import JohnH from '../../assets/about/johnH.png';
 import Phillip from '../../assets/about/phillip.png';
+import { Link } from "react-router-dom";
 
 
 const About = () => {
@@ -16,31 +17,39 @@ const About = () => {
         <>
             <div className="top-level-div">
                 <div className="main-containerz">
-                    <h1 className="aboutNames">About Us</h1>
+                    <h1 className="aboutNames">About The Team</h1>
                     <div className="row">
                         <div className="col">
                             <div className="aboutMe">
                                 <div className="aboutText">
-                                    <h3 className="aboutNames">Austin</h3>
+                                    <h3 className="aboutNames">Austin Donovan</h3>
                                     <img src={Austin} className="aboutMe-img" alt="Austin" />
-                                    <p className="aboutDescriptions">Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsam corrupti iste, earum nulla ut! Dignissimos accusamus dolorum omnis, aperiam distinctio maiores commodi officia. Rerum quaerat deleniti culpa eius dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsam corrupti iste, earum nulla ut! Dignissimos accusamus dolorum omnis, aperiam distinctio maiores commodi officia. Rerum quaerat deleniti culpa eius dolor.</p>
+                                    <p className="aboutDescriptions">Likes league. Goes to nightclubs.</p>
+                                    <div className="about-linkz"><a href="https://github.com/AustinLD" target="_blank">Github link</a></div>
+                                    <div className="about-linkz"><a href="https://www.linkedin.com/in/austin-donovan-b39151102/" target="_blank">LinkedIn link</a></div>
                                     {/* ------------------------------------------ */}
-                                    <h3 className="aboutNames">John G.</h3>
+                                    <h3 className="aboutNames">John Guzzetta</h3>
                                     <img src={JohnG} className="aboutMe-img" alt="JohnG" />
-                                    <p className="aboutDescriptions">Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsam corrupti iste, earum nulla ut! Dignissimos accusamus dolorum omnis, aperiam distinctio maiores commodi officia. Rerum quaerat deleniti culpa eius dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsam corrupti iste, earum nulla ut! Dignissimos accusamus dolorum omnis, aperiam distinctio maiores commodi officia. Rerum quaerat deleniti culpa eius dolor.</p>
+                                    <p className="aboutDescriptions">Also likes league. Has cats.</p>
+                                    <div className="about-linkz"><a href="https://github.com/guzzemetti" target="_blank">Github link</a></div>
+                                    <div className="about-linkz"><a href="https://www.linkedin.com/in/johnguzzettaiv/" target="_blank">LinkedIn link</a></div>
                                 </div>
                             </div>
                         </div>
                         <div className="col">
                             <div className="aboutMe">
                                 <div className="aboutText">
-                                    <h3 className="aboutNames">John H.</h3>
+                                    <h3 className="aboutNames">John Hysong</h3>
                                     <img src={JohnH} className="aboutMe-img" alt="JohnH" />
-                                    <p className="aboutDescriptions">Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsam corrupti iste, earum nulla ut! Dignissimos accusamus dolorum omnis, aperiam distinctio maiores commodi officia. Rerum quaerat deleniti culpa eius dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsam corrupti iste, earum nulla ut! Dignissimos accusamus dolorum omnis, aperiam distinctio maiores commodi officia. Rerum quaerat deleniti culpa eius dolor.</p>
+                                    <p className="aboutDescriptions">Does not like league. Is tall.</p>
+                                    <div className="about-linkz"><a href="https://github.com/guzzemetti" target="_blank">Github link</a></div>
+                                    <div className="about-linkz"><a href="https://www.linkedin.com/in/johnguzzettaiv/" target="_blank">LinkedIn link</a></div>
                                     {/* ------------------------------------------ */}
-                                    <h3 className="aboutNames">Phillip</h3>
+                                    <h3 className="aboutNames">Phillip Pereira</h3>
                                     <img src={Phillip} className="aboutMe-img" alt="Phillip" />
-                                    <p className="aboutDescriptions">Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsam corrupti iste, earum nulla ut! Dignissimos accusamus dolorum omnis, aperiam distinctio maiores commodi officia. Rerum quaerat deleniti culpa eius dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsam corrupti iste, earum nulla ut! Dignissimos accusamus dolorum omnis, aperiam distinctio maiores commodi officia. Rerum quaerat deleniti culpa eius dolor.</p>
+                                    <p className="aboutDescriptions">Works out. Not sure if he likes league.</p>
+                                    <div className="about-linkz"><a href="https://github.com/guzzemetti" target="_blank">Github link</a></div>
+                                    <div className="about-linkz"><a href="https://www.linkedin.com/in/johnguzzettaiv/" target="_blank">LinkedIn link</a></div>
                                 </div>
                             </div>
                         </div>
