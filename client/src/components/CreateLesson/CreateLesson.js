13fae33613df958e4346904a19e92b1ee6f91b63
John Guzzetta <johnguzzettaiv@gmail.com>
2022-06-09 01:12:57 -0400

    finished add lesson modal

--- client/src/components/CreateLesson/CreateLesson.js
+++ client/src/components/CreateLesson/CreateLesson.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './createModal.css'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { Label, Input, Row, Col, FormGroup, Form } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, InputGroup } from 'reactstrap';
+import { Label, Input, Row, Col, FormGroup, Form, InputGroupText } from 'reactstrap';
 
 
 class ModalExample extends React.Component {
@@ -24,95 +24,103 @@ class ModalExample extends React.Component {
 
   render() {
     return (
-        <div>
+      <div>
         <Button className="buttonColor" onClick={this.toggle}>{this.props.buttonLabel}Create Lesson</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-          <ModalBody>
-          <Form>
-                    <Row>
-                      <div className="inputRow">
-                        {/* Email Input */}
-                        <Col md={12}>
-                          <FormGroup >
-                            <Label className="labels" for="exampleEmail">
-                              Email
-                            </Label>
-                            <Input
-                              id="exampleEmail"
-                              name="email"
-                              placeholder="Email Address"
-                              type="email"
-                            />
-                          </FormGroup>
-                        </Col>
-                        {/* Password Input */}
-                        <Col md={12}>
-                          <FormGroup>
-                            <Label className="labels" for="examplePassword">
-                              Password
-                            </Label>
-                            <Input
-                              id="examplePassword"
-                              name="password"
-                              placeholder="******"
-                              type="password"
-                            />
-                          </FormGroup>
-                        </Col>
-                        {/* First Name Input */}
-                        <Row>
-                          <Col md={6}>
-                            <FormGroup>
-                              <Label className="labels" for="firstName">
-                                First Name
-                              </Label>
-                              <Input
-                                id="firstName"
-                                name="firstname"
-                                placeholder="First Name"
-                                type="textarea"
-                              />
-                            </FormGroup>
-                          </Col>
-                          {/* Last Name Input */}
-                          <Col md={6}>
-                            <FormGroup>
-                              <Label className="labels" for="lastName">
-                                Last Name
-                              </Label>
-                              <Input
-                                id="lastName"
-                                name="lastname"
-                                placeholder="Last Name"
-                                type="textarea"
-                              />
-                            </FormGroup>
-                          </Col>
-                        </Row>
-                        {/* About Me Input */}
-                        <Col md={12}>
-                          <FormGroup>
-                            <Label className="labels" for="aboutMe">
-                              About Me
-                            </Label>
-                            <Input
-                              id="aboutMe"
-                              name="aboutMe"
-                              placeholder="Type Here..."
-                              type="textarea"
-                            />
-                          </FormGroup>
-                        </Col>
-                        <button className="card-button" type="submit">Get Started</button>
-                      </div>
-                    </Row>
-                    
-                  </Form>
+          <h3 id="modal-header">Create a Lesson</h3>
+          <ModalBody className="modal-body">
+            {/* The below form is the form that is held within the modal */}
+          <div>
+            <Form>
+              <Row>
+                <div className="inputRow">
+                  {/* Title name input */}
+                  <Col md={12}>
+                    <FormGroup >
+                      <Label className="labels" for="lessonTitle">
+                        Lesson Title
+                      </Label>
+                      <Input
+                        id="lessonTitle"
+                        name="lessonName"
+                        placeholder="Lesson name"
+                        type="title"
+                      />
+                    </FormGroup>
+                  </Col>
+                  {/* Description input */}
+                  <Col md={12}>
+                    <FormGroup>
+                      <Label className="labels" for="lessonDescription">
+                        Description
+                      </Label>
+                      <Input
+                        id="lessonDescription"
+                        name="lessonDesc"
+                        placeholder="Enter a brief description for this lesson"
+                        type="textarea"
+                      />
+                    </FormGroup>
+                  </Col>
+                  {/* Drop-down to select the Category */}
+                  <Row>
+                    <Col md={6}>
+                      <FormGroup>
+                        <Label className="labels" for="categoryDrop">
+                          Category
+                        </Label>
+                        <Input
+                          id="categoryDrop"
+                          name="catDrop"
+                          placeholder="Select a Category"
+                          type="select">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                        </Input>
+                      </FormGroup>
+                    </Col>
+                    {/* Dropdown to select Sub Category */}
+                    <Col md={6}>
+                      <FormGroup>
+                        <Label className="labels" for="subcatDrop">
+                          Sub-Category
+                        </Label>
+                        <Input
+                          id="subcatDrop"
+                          name="subDrop"
+                          placeholder="Select a Sub-Category"
+                          type="select">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                        </Input>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  {/* Price Input */}
+                  <Col md={12}>
+                    <FormGroup>
+                      <Label className="labels" for="priceInput">
+                        Pricing
+                      </Label>
+                      <InputGroup id="priceInput">
+                        <Input placeholder="Enter price" type="number" />
+                        <InputGroupText>
+                          .00
+                        </InputGroupText>
+                      </InputGroup>
+                    </FormGroup>
+                  </Col>
+                  {/* <button className="card-button" type="submit">Create Lesson!</button> */}
+                </div>
+              </Row>
+            </Form>
+            </div>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <Button className="buttonColor" onClick={this.toggle}>Create Lesson!</Button>{' '}
           </ModalFooter>
         </Modal>
       </div>
@@ -123,11 +131,11 @@ class ModalExample extends React.Component {
 
 // Do props need to be passed in?
 const CreateLesson = () => {
-    return (
-      <>
-          <ModalExample />  
-      </>
-    )
-  }
-  
-  export default CreateLesson;
+  return (
+    <>
+      <ModalExample />
+    </>
+  )
+}
+
+export default CreateLesson;
