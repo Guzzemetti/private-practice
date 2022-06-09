d4948d6cf538c7adce62d28dd4246c95c0126c49
Phillip48 <phillippereira02@gmail.com>
2022-06-08 23:12:04 -0400

    Starter code for mapping card

--- client/src/components/categoryCards/index.js
+++ client/src/components/categoryCards/index.js
@@ -7,39 +7,47 @@ import tempPic from '../../assets/blank-profile-picture-973460__340.webp';
 // import Cat1 from '../../assets/bg_gaming.jpg';
 
 // import LogoHeader from '../../assets/Private Practice-logos_transparent.png';
-
-const Category = () => {
-
+//  title, description, price, coach, review, subcategory
+const Category = ({ Lesson }) => {
+    if (!Lesson) {
+        return <h3>No Lesson</h3>;
+    }
     // In Category page Map over everything and pass props to get all the instances of whats here from the DB
     // Img height is in px might have to chnage to make it more dynamic
     return (
-        
+
         <section className="section-hold-all-caetgories-options">
             <div className="category-header-with-img">
                 <h1>The Category they choose</h1>
             </div>
             <div className="holds-cards">
-                <Card className="card-margin-top">
-                    <CardBody>
-                        <CardTitle tag="h5">
-                            {/* Replace with dynamic title */}
-                            Categories will go here
-                        </CardTitle>
-                        {/* This might not be needed */}
-                        <CardSubtitle className="mb-2 text-muted" tag="h6">
-                            Card subtitle
-                        </CardSubtitle>
-                        <CardText className="card-text-with-img">
-                            <div className="card-text-side">
-                                Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.  Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.
-                            </div>
-                            <div className="card-img-side">
-                                <img src={tempPic} className='img-width-for-profile' alt='Profile img'></img>
-                            </div>
-                        </CardText>
-                        <button className="card-button">Add to Cart</button>
-                    </CardBody>
-                </Card>
+                {Lesson &&
+                    Lesson.map((lessons) => (
+                        <Card className="card-margin-top">
+                            <CardBody>
+                                <CardTitle tag="h5">
+                                    {/* Replace with dynamic title */}
+                                    {lessons.title} 
+                                    {lessons.coach}
+                                    {lessons.price}
+                                </CardTitle>
+                                {/* This might not be needed */}
+                                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                                    {lessons.category}
+                                    {lessons.subcategory}
+                                </CardSubtitle>
+                                <CardText className="card-text-with-img">
+                                    <div className="card-text-side">
+                                        {lessons.description}
+                                    </div>
+                                    <div className="card-img-side">
+                                        <img src={tempPic} className='img-width-for-profile' alt='Profile img'></img>
+                                    </div>
+                                </CardText>
+                                <button className="card-button">Add to Cart</button>
+                            </CardBody>
+                        </Card>
+                    ))}
             </div>
 
         </section>
