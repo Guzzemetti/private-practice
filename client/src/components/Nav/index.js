f5c4dd51839ccf76cd732f7a30c307c8542af241
Phillip48 <phillippereira02@gmail.com>
2022-06-10 00:38:04 -0400

    Took a link out of navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -38,8 +38,6 @@ const Navbar = () => {
               <div className="loggedin-or-not-navbar">
                 <Link to='/about'><button className="navbar-button">About</button></Link>
                 <div className="div-padding"></div>
-                <CreateLesson />
-                <div className="div-padding"></div>
                 <Link to={`/profile/${Auth.getProfile().data._id} `}><button className="navbar-button">Profile</button></Link>
                 <div className="div-padding"></div>
                 <a href="/" className="navbar-button" onClick={() => Auth.logout()}>Logout</a>
